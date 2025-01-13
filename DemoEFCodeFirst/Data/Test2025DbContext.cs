9230a535899bff9c905ccba8bc80b06ba0470f12
hvu26510 <hvu26510@gmail.com>
2025-01-13 10:17:02 +0700

    Add project files.

---
+++ DemoEFCodeFirst/Data/Test2025DbContext.cs
@@ -0,0 +1,15 @@
+﻿using DemoEFCodeFirst.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoEFCodeFirst.Data
+{
+    public class Test2025DbContext : DbContext
+    {
+        public Test2025DbContext(DbContextOptions<Test2025DbContext> options) 
+            : base(options) 
+        { 
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+    }
+}
